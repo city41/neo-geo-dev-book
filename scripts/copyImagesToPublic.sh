dbab034d4e88757b72d6b9a878ba6c1a987192b4
Matt Greer <matt.e.greer@gmail.com>
2022-01-12 16:08:24 -0500

    fix find/public script

--- scripts/copyImagesToPublic.sh
+++ scripts/copyImagesToPublic.sh
@@ -1,4 +1,4 @@
 # copy all images in the book directory over to public, this enables
 # the markdown files in the book directory to work as-is, such as vs code preview
 # or looking at the file on github. But also lets them work on the website
-find book -iname '*.{svg,png,jpeg,jpg,gif}' -exec cp '{}' public/ \;
+find ./book -type f \( -iname \*.jpg -o -iname \*.jpeg -o -iname \*.png -o -iname \*.svg -o -iname \*.gif \) -exec cp '{}' public/ \;
