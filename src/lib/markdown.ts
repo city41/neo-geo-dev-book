58cdb5e0db4d578c2328cc336d5e46d67ab91abc
Matt Greer <matt.e.greer@gmail.com>
2022-01-12 09:41:23 -0500

    add remark-gfm for footnotes

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,11 +1,13 @@
 import { remark } from 'remark';
 import html from 'remark-html';
 import prism from 'remark-prism';
+import gfm from 'remark-gfm';
 
 export default async function markdownToHtml(
 	markdown: string
 ): Promise<string> {
 	// @ts-ignore prism seems to not quite meet use()'s expected param type
-	const result = await remark().use(html).use(prism).process(markdown);
+	const result = await remark().use(html).use(prism).use(gfm).process(markdown);
+
 	return result.toString();
 }
