4168afe86729eafcf76f27592ee9cee0512e7d55
Matt Greer <matt.e.greer@gmail.com>
2022-01-30 10:10:05 -0500

    index: book structure blurb

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,6 +31,17 @@ export default function Home() {
 					Only an HTML version for now, but once the book is somewhat decent
 					I'll have pdf, mobi and epub versions available here too.
 				</p>
+				<h2 className="text-2xl font-bold my-4">Book Structure</h2>
+				<p>
+					The main chapters of the book build out a simple game of breakout and
+					cover the major aspects of the Neo Geo&apos;s hardware and coding for
+					it. From there, the appendices dive into deeper subjects and are more
+					standalone. Once you are comfortable with the basics of the Neo Geo,
+					you can pick and choose to read appendices as they spark your
+					interest. I realize that isn&apos;t normally how an "appendix" works,
+					but that&apos;s what I&apos;m going with so far. Maybe I&apos;ll
+					rename them "supplementals"?
+				</p>
 			</main>
 		</div>
 	);
